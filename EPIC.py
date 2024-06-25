d37d58f7980289645a9ddfaad2895df43e0877bf
Stmkv <stepkamakarov1999@mail.ru>
2024-06-25 20:00:43 +0300

    download photos from NASA.

---
+++ EPIC.py
@@ -0,0 +1,41 @@
+import requests
+import os
+import urllib
+import shutil
+import datetime
+
+
+def download_photos_nasa_epic(count = 5):
+    photos_key = []
+    photos_date = []
+    response = requests.get("https://api.nasa.gov/EPIC/api/natural/images?api_key=DEMO_KEY")
+    response.raise_for_status()
+    response = response.json()
+    for i in response:
+        photos_key.append(i["image"])
+        photos_date.append(i["date"])
+
+    for i, w in enumerate(photos_date):
+        photos_date[i] = datetime.datetime.strptime(w, "%Y-%m-%d %H:%M:%S").strftime("%Y/%m/%d")
+
+    for i in range(0, count):
+        url = (f"https://api.nasa.gov/EPIC/archive/natural/{photos_date[i]}/png/{photos_key[i]}.png")
+        download_picture(url, f"nasa_epic{i+1}.jpg")
+
+
+
+def download_picture(
+    url: str,
+    picture_path: str,
+) -> None:
+    params = {"api_key": "uBdF2I4UKAoqADW607qz73gjPAerS8a8f9dyjVuR"}
+    response = requests.get(url,params=params)
+    response.raise_for_status()
+
+    with open(picture_path, "wb") as file:
+        file.write(response.content)
+    shutil.move(picture_path, "images")  # Перемещение фотографии в папку images
+
+
+
+download_photos_nasa_epic(5)
---
+++ SPACEX.py
@@ -0,0 +1,36 @@
+import requests
+import os
+import shutil
+import urllib
+
+
+def cerate_folder_for_images() -> None:
+    if not os.path.exists("images"):
+        os.makedirs("images")
+
+
+def download_picture(
+    url: str,
+    picture_path: str,
+) -> None:
+    response = requests.get(url)
+    response.raise_for_status()
+
+    with open(picture_path, "wb") as file:
+        file.write(response.content)
+    shutil.move(picture_path, "images")  # Перемещение фотографии в папку images
+
+
+def fetch_spacex_last_launch() -> None:
+    response = requests.get("https://api.spacexdata.com/v5/launches/5eb87d47ffd86e000604b38a")
+    response.raise_for_status()
+
+    response = response.json()
+    for number, pictures in enumerate(response["links"]["flickr"]["original"]):
+        download_picture(pictures, f"spacex{number}.jpg")
+
+
+if __name__ == "__main__":
+    cerate_folder_for_images()
+    fetch_spacex_last_launch()
+s
