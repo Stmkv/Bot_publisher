9f29f63b4825bd91ee5b4af6370b4aa461d0ae2c
Stmkv <stepkamakarov1999@mail.ru>
2024-06-26 14:50:13 +0300

    Create auto_publisher
    Написал скрипт для автопубликации фото
    
    Поправил requriments.txt

---
+++ auto_publisher_bot.py
@@ -0,0 +1,47 @@
+import telegram
+import os
+import random
+import time
+import argparse
+from dotenv import load_dotenv
+
+
+def create_parser():
+    parser = argparse.ArgumentParser(
+        description="Публикует заданное кол-во фотографий за раз."
+    )
+    parser.add_argument(
+        "count",
+        help="Введите количество фотографий, которые\
+                        нужно опубликовать",
+    )
+    args = parser.parse_args()
+    return args.count
+
+
+def auto_public_photo(bot, chat_id, img_list, count=1) -> None:
+    for i in range(count):
+        if not img_list:  # Если список пуст, перемешиваем его заново
+            img_list.extend(os.listdir("images/"))
+            random.shuffle(img_list)
+
+        photo_path = f"images/{img_list.pop()}"  # Получаем и удаляем последний элемент списка
+        with open(photo_path, 'rb') as photo:
+            bot.send_photo(chat_id, photo=photo)
+
+
+if __name__ == "__main__":
+    count_photo_for_publication = int(create_parser())
+    load_dotenv()
+    telegram_bot_token = os.environ["TELEGRAM_BOT_TOKEN"]
+    telegram_chat_id = os.environ["TELEGRAM_CHAT_ID"]
+    publication_frequency = os.environ["PUBLICATION_FREQUENCY"]
+    photos_per_cycle = os.environ.get("PHOTOS_PER_CYCLE", 1)  # По умолчанию 1 фото
+
+    bot = telegram.Bot(token=telegram_bot_token)
+    img_list = os.listdir("images/")
+    random.shuffle(img_list)
+
+    while True:
+        auto_public_photo(bot, telegram_chat_id, img_list, count=count_photo_for_publication)
+        time.sleep(float(publication_frequency))
--- publisher_bot.py
+++ publisher_bot.py
@@ -1,10 +1,19 @@
 import telegram
 import os
+import random
+
 from dotenv import load_dotenv
 
-load_dotenv()
-telegram_bot_token = os.environ["TELEGRAM_BOT_TOKEN"]
-telegram_chat_id = os.environ["TELEGRAM_CHAT_ID"]
 
-bot = telegram.Bot(token=telegram_bot_token)
-bot.send_message(chat_id=telegram_chat_id, text="I'm sorry Dave I'm afraid I can't do that.")
+def public_photo(chat_id):
+    img_list = os.listdir(r"images/")
+    photo_path = f"images/{random.choice(img_list)}"
+    bot.send_photo(chat_id, photo=open(photo_path, 'rb'))
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    telegram_bot_token = os.environ["TELEGRAM_BOT_TOKEN"]
+    telegram_chat_id = os.environ["TELEGRAM_CHAT_ID"]
+    bot = telegram.Bot(token=telegram_bot_token)
+    public_photo(telegram_chat_id)
