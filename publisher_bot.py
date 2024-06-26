50d3b5df4714b163958754c2a16ef375ef5b7e89
Stmkv <stepkamakarov1999@mail.ru>
2024-06-26 15:40:57 +0300

    repaqir_publisher_bot

--- publisher_bot.py
+++ publisher_bot.py
@@ -1,19 +1,39 @@
 import telegram
 import os
 import random
-
+import argparse
 from dotenv import load_dotenv
 
 
-def public_photo(chat_id):
-    img_list = os.listdir(r"images/")
-    photo_path = f"images/{random.choice(img_list)}"
-    bot.send_photo(chat_id, photo=open(photo_path, 'rb'))
+def create_parser():
+    parser = argparse.ArgumentParser(
+        description="Публикует выбранную фотографию."
+    )
+    parser.add_argument(
+        "img",
+        nargs="?",
+        help="Введите фотографию, которую\
+                        нужно опубликовать",
+        default=None
+    )
+    args = parser.parse_args()
+    return args.img
+
+
+def public_photo(chat_id, img_list, img = None):
+    if img == None:
+        photo_path = f"images/{random.choice(img_list)}"
+        bot.send_photo(chat_id, photo=open(photo_path, 'rb'))
+    else:
+        photo_path = f"images/{img}"
+        bot.send_photo(chat_id, photo=open(photo_path, 'rb'))
 
 
 if __name__ == "__main__":
     load_dotenv()
+    img = create_parser()
+    img_list = os.listdir(r"images/")
     telegram_bot_token = os.environ["TELEGRAM_BOT_TOKEN"]
     telegram_chat_id = os.environ["TELEGRAM_CHAT_ID"]
     bot = telegram.Bot(token=telegram_bot_token)
-    public_photo(telegram_chat_id)
+    public_photo(telegram_chat_id, img_list, img)
